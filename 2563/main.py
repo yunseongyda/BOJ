71837157ff725c8609ac1583c84fc338e99daf87
yunseongyda <kys070510@gmail.com>
2025-03-18 21:00:42 +0900

    성공쓰

--- 2563/main.py
+++ 2563/main.py
@@ -1,47 +1,9 @@
-def put_paper(ta, n):
-  papers[n] = list(map(int, input().split()))
-  ta += 100
-  return ta
+board = [[0]*100 for _ in range(100)]
+papers = [list(map(int, input().split())) for _ in range(int(input()))]
 
-def checkOverlap(rec, prev):
-  x1, y1 = rec[0], rec[1]
-  x2, y2 = prev[0], prev[1]
-  if x2 >= x1+10 or y2 >= y1+10 or x2+10 <= x1 or y2+10 <= y1:
-    return 0
-  return 1
+for x, y in papers:
+  for i in range(x, x+10):
+    for j in range(y, y+10):
+      board[i][j] = 1
 
-def get_x(rec, prev):
-  x1, y1 = rec[0], rec[1]
-  x2, y2 = prev[0], prev[1]
-  if x1 < x2:
-    x = x1+10 - x2
-  elif x1 > x2:
-    x = x2+10 - x1
-  elif x1 == x2:
-    x = x1+10 - x1
-  return x
-
-def get_y(rec, prev):
-  x1, y1 = rec[0], rec[1]
-  x2, y2 = prev[0], prev[1]
-  if y1 < y2:
-    y = y1+10 - y2
-  elif y1 > y2:
-    y = y2+10 - y1
-  elif y1 == y2:
-    y = y1+10 - y1
-  return y
-
-total_area = 0
-papers = []
-N = int(input())
-papers = [[0,0] for i in range(N)]
-total_area = put_paper(total_area, 0)
-
-for i in range(1, N):
-  total_area = put_paper(total_area, i)
-  for j in range(1, i+1):
-    if checkOverlap(papers[-1], papers[-j-1]):
-      total_area -= get_x(papers[-1], papers[-j-1]) * get_y(papers[-1], papers[-j-1])
-    
-print(total_area)
+print(sum(sum(i) for i in board))
